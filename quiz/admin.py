1760a7e4cff5e93f46d00e9fe0f987146c07718e
parthsujalshah <parthsujalshah@gmail.com>
2021-07-30 16:46:35 +0530

    queries

---
+++ quiz/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Answer, Category, Question, Quizzes
+
+admin.site.register(Answer)
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Quizzes)
---
+++ quiz/schema.py
@@ -0,0 +1,37 @@
+import graphene
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import Quizzes, Category, Question, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ('id', 'title', 'category', 'quiz')
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ('id', 'title', 'quiz', 'answer')
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ('id', 'question', 'answer_text')
+
+
+class Query(graphene.ObjectType):
+
+    # all_quizzes = DjangoListField(QuizzesType) # This doesn't require resolver function
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+    all_answers = graphene.List(AnswerType, id=graphene.Int())
+
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.filter(question=id)
+
+schema = graphene.Schema(query=Query)
